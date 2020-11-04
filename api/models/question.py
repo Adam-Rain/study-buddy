af25a8d0526bdb74b65f6d7890b851f7b9583694
adamrturman <adamrturman@gmail.com>
2020-11-04 11:50:24 -0600

    created question model

---
+++ api/models/question.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create question model
+class Question(models.Model):
+  question = models.CharField(max_length=500)
+  answer = models.CharField(max_length=500)
+  question_set = models.ForeignKey(
+    'Question Set',
+    related_name='questions',
+    on_delete=models.CASCADE
+  )
+
+  def __str__(self):
+    return f"Here is the question: {self.question}. Here is the answer: {self.answer}."
+
+  def as_dict(self):
+    return {
+    'id': self.id,
+    'question': self.question,
+    'answer': self.answer
+    }

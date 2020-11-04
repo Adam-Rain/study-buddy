bd632221fd3492e45ce21aafdc90f2ceae6a5ea1
rainswerld <m.rainrobinson@gmail.com>
2020-11-04 11:15:48 -0700

    Create url patterns for Question table

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 # from .views.mango_views import Mangos, MangoDetail
 from .views.question_set_views import QuestionSets, QuestionSetDetail
+from .views.question_views import Question, QuestionDetail
 from .views.user_views import SignUp, SignIn, SignOut, ChangePassword
 
 urlpatterns = [
@@ -9,6 +10,8 @@ urlpatterns = [
     # path('mangos/<int:pk>/', MangoDetail.as_view(), name='mango_detail'),
     path('question_sets/', QuestionSets.as_view(), name='question_sets'),
     path('question_sets/<int:pk>', QuestionSetDetail.as_view(), name='question_set_detail'),
+    path('questions/', Question.as_view(), name='questions'),
+    path('question/<int:pk>', QuestionDetail.as_view(), name='question'),
     path('sign-up/', SignUp.as_view(), name='sign-up'),
     path('sign-in/', SignIn.as_view(), name='sign-in'),
     path('sign-out/', SignOut.as_view(), name='sign-out'),

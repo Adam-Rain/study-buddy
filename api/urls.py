44618de63ad68594084b39d2f084eae25b8a9a18
adamrturman <adamrturman@gmail.com>
2020-11-04 11:18:05 -0600

    added question set urls to urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 # from .views.mango_views import Mangos, MangoDetail
+from .views.question_set_views import QuestionSets, QuestionSetDetail
 from .views.user_views import SignUp, SignIn, SignOut, ChangePassword
 
 urlpatterns = [
   	# Restful routing
     # path('mangos/', Mangos.as_view(), name='mangos'),
     # path('mangos/<int:pk>/', MangoDetail.as_view(), name='mango_detail'),
+    path('question_sets/', QuestionSets.as_view(), name='question_sets'),
+    path('question_sets/<int:pk>', QuestionSetDetail.as_view(), name='question_set_detail'),
     path('sign-up/', SignUp.as_view(), name='sign-up'),
     path('sign-in/', SignIn.as_view(), name='sign-in'),
     path('sign-out/', SignOut.as_view(), name='sign-out'),

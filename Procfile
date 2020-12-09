70b17e9c9be8bc298e10dcf697a243fef7c1e83f
adamrturman <adamrturman@gmail.com>
2020-12-09 11:09:50 -0600

    deploy

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn study-buddy.wsgi
+release: python manage.py migrate
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'study_buddy.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'study-buddy.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
